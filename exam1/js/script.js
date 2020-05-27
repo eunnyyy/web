938d9bafb50f21259eadb9ae2f153768139813e5
eunnyyy <jse1282@gmail.com>
2020-05-27 12:16:26 +0900

    ss
    s

--- exam1/js/script.js
+++ exam1/js/script.js
@@ -29,3 +29,34 @@ setInterval(function(){
     $(".slide_box").delay("2500");
     $(".slide_box").animate({marginTop:"0"},"500");
 })
+
+
+//tab
+$(function(){
+    $(".tab-btn1").click(function(){
+        $(".tab-con1").css("display", "block");
+        $(".tab-con2").css("display", "none");
+        $(".tab-btn1").css("background", "lightseagreen");
+        $(".tab-btn2").css("background", "none");
+        $(".tab-con1").css("background", "lightseagreen");
+        $(".tab-con2").css("background", "none");
+    });
+    $(".tab-btn2").click(function(){
+        $(".tab-con1").css("display", "none");
+        $(".tab-con2").css("display", "block");
+        $(".tab-btn1").css("background", "none");
+        $(".tab-btn2").css("background", "lightseagreen");
+        $(".tab-con1").css("background", "none");
+        $(".tab-con2").css("background", "lightseagreen");
+        
+    });
+});
+
+$(function(){
+    $("#popup_button").on('click', function(){
+        $('.popup').fadeIn();
+    });
+    $(".팝업닫기").on('click', function(){
+        $('.popup').fadeOut();
+    });
+});
