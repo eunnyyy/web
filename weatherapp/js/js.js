5fe54f341e1c1c38e1dbb2927302d24ab5a4c2e4
alswjd1125 <alslal1125@naver.com>
2020-08-10 12:53:31 +0900

    qq
    q

---
+++ weatherapp/js/js.js
@@ -0,0 +1,12 @@
+// var today= new Date();
+// var nowMonth=today.getMonth()+1;
+// var nowDate=today.getDate();
+// var nowDay=today.getDay();
+// var nowHours=today.getHours();
+// var nowMinutes=today.getMinutes();
+
+// document.write("월: "+nowMonth+"<br>");
+// document.write("일: "+nowDate+"<br>");
+// document.write("요일: "+nowDay+"<br>");
+// document.write("시: "+nowHours+"<br>");
+// document.write("분: "+nowMinutes+"<br>");tt 
